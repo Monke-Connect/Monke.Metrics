409b1e8f7e383fd330670f48bac3332c4f56e580
lh <lhillen@oas.de>
2026-07-16 17:34:25 +0200

    Program setup and db file created

---
+++ server/src/Monke.Metrics/Data/MetricsDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Monke.Metrics.Data
+{
+	public class MetricsDbContext(DbContextOptions<MetricsDbContext> options) : DbContext(options)
+	{
+	}
+}
--- server/src/Monke.Metrics/Program.cs
+++ server/src/Monke.Metrics/Program.cs
@@ -1,15 +1,77 @@
+using System.Threading.RateLimiting;
+
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
+
+using Monke.Metrics.Data;
+
 namespace Monke.Metrics
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			var builder = WebApplication.CreateBuilder(args);
-			var app = builder.Build();
+			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+			_ = builder.Services.AddControllers();
+			_ = builder.Services.AddHealthChecks();
+			ConfigureRateLimiting(builder);
+			ConfigureLogging(builder);
+			ConfigureDatabaseContext(builder);
 
-			app.MapGet("/", () => "Hello World!");
+			_ = builder.Services.AddCors(options =>
+				options.AddPolicy("Network", policy =>
+				{
+					_ = policy
+						.AllowAnyOrigin()
+						.AllowAnyHeader()
+						.AllowAnyMethod();
+				})
+			);
+
+			WebApplication app = builder.Build();
+			_ = app.UseCors("Network");
+			_ = app.UseRateLimiter();
+			_ = app.MapHealthChecks("monke/metrics/health");
+			_ = app.MapControllers().RequireRateLimiting("fixed");
+
+			// Migrationen anwenden
+			using (IServiceScope scope = app.Services.CreateScope())
+			{
+				MetricsDbContext dbContext = scope.ServiceProvider.GetRequiredService<MetricsDbContext>();
+				dbContext.Database.Migrate();
+			}
 
 			app.Run();
+
+		}
+
+
+		private static void ConfigureRateLimiting(WebApplicationBuilder builder)
+		{
+			_ = builder.Services.AddRateLimiter(options =>
+			{
+				_ = options.AddFixedWindowLimiter("fixed", opt =>
+				{
+					opt.PermitLimit = 4;
+					opt.Window = TimeSpan.FromSeconds(12);
+					opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+					opt.QueueLimit = 2;
+				});
+			});
+		}
+
+		private static void ConfigureLogging(WebApplicationBuilder builder)
+		{
+			// Logging
+			_ = builder.Logging.ClearProviders();
+			_ = builder.Logging.AddConsole();
+		}
+
+		private static void ConfigureDatabaseContext(WebApplicationBuilder builder)
+		{
+			_ = builder.Services.AddDbContext<MetricsDbContext>(options =>
+				_ = options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+			);
 		}
 	}
 }
